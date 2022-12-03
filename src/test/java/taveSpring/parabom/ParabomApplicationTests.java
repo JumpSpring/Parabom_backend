96c876447defc410502f82c2317226681b67b6ff
jwPark <1211abc@naver.com>
2022-12-03 16:54:26 +0900

    init parabom

---
+++ src/test/java/taveSpring/parabom/ParabomApplicationTests.java
@@ -0,0 +1,13 @@
+package taveSpring.parabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParabomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
