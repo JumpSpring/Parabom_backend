916645e8476268d72c9099d35817e6edbaee1591
Jeongyoon Song <sjyjudy710@naver.com>
2022-12-25 03:27:22 +0900

    fix: 게시물 상세조회 findByIdBy 쿼리 수정

--- src/main/java/taveSpring/parabom/Repository/ImageRepository.java
+++ src/main/java/taveSpring/parabom/Repository/ImageRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
-    @Query("FROM Image i JOIN Post p WHERE p.id = ?1")
+    @Query("SELECT i FROM Image i JOIN i.post p WHERE p.id = ?1")
     List<Image> findByIdBy(Long id); // 게시물 id로 해당 게시물 이미지리스트 불러오기
 
 }
