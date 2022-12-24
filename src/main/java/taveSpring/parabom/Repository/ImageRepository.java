78c50a651246da283b0eb007a2c2a82edd44b8b5
Jeongyoon Song <sjyjudy710@naver.com>
2022-12-24 17:41:13 +0900

    fix: 게시물 등록 시 이미지 첨부 수정 / feat: 게시물 상세조회 일부

--- src/main/java/taveSpring/parabom/Repository/ImageRepository.java
+++ src/main/java/taveSpring/parabom/Repository/ImageRepository.java
@@ -1,9 +1,16 @@
 package taveSpring.parabom.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import taveSpring.parabom.Domain.Image;
+import taveSpring.parabom.Domain.Post;
+
+import java.util.List;
 
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query("FROM Image i JOIN Post p WHERE p.id = ?1")
+    List<Image> findByPostId(Long id); // 게시물 id로 해당 게시물 이미지리스트 불러오기
 
 }
--- src/main/java/taveSpring/parabom/Service/PostService.java
+++ src/main/java/taveSpring/parabom/Service/PostService.java
@@ -2,13 +2,19 @@ package taveSpring.parabom.Service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import taveSpring.parabom.Controller.Dto.ImageDto;
 import taveSpring.parabom.Controller.Dto.PostDto;
+import taveSpring.parabom.Domain.Image;
 import taveSpring.parabom.Domain.Member;
 import taveSpring.parabom.Domain.Post;
+import taveSpring.parabom.Repository.ImageRepository;
 import taveSpring.parabom.Repository.PostRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,17 +23,56 @@ import java.util.stream.Stream;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final PostImageService postImageService;
+    private final ImageRepository imageRepository;
 
     /*게시물 상세조회*/
+    @Transactional
     public PostDto.PostDetailDto productDetail(Long id) {
-        Post entity = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("게시물이 존재하지 않습니다."));
+
+        // 이미지
+        List<Image> imageList = imageRepository.findByPostId(id);
+        List<ImageDto.ImageInfoDto> imageDtoList = new ArrayList<>();
+
+        for (Image images : imageList) {
+            ImageDto.ImageInfoDto dto = ImageDto.ImageInfoDto.of(images);
+            imageDtoList.add(dto);
+        }
+
+        // 게시물 내용
+        Post entity = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("게시물을 찾을 수 없습니다."));
+        PostDto.PostDetailDto detailDto = new PostDto.PostDetailDto(entity);
+        detailDto.setImageDtoList(imageDtoList);
+        System.out.print("[Info][productDetail SERVICE] detailDto.getImageInfoDtos() : " + detailDto.getImageDtoList());
         return new PostDto.PostDetailDto(entity);
     }
 
     /*게시물 등록*/
     @Transactional
-    public Long postCreate(PostDto.PostCreateDto dto) {
-        return postRepository.save(dto.toEntity()).getId();
+    public Long postCreate(PostDto.PostCreateDto postDto, List<MultipartFile> imageList) throws Exception {
+
+        // 게시물 내용 등록
+        //Post post = postRepository.save(postDto.toEntity());
+
+        // 이미지 등록
+        for(int i=0 ; i<imageList.size(); i++){
+            Image image = new Image();
+            //image.setPost(post);
+            postImageService.saveImage(image, imageList.get(i));
+            System.out.println("[Info][productCreate SERVICE] image.getPath() : " + image.getPath());
+
+            Post postE = new Post(); //new
+            postE.addImages(image); //new
+            postDto.setImage(image);
+        }
+
+        // 게시물 내용 등록
+        Post post = postRepository.save(postDto.toEntity());
+
+        // 이미지 엔티티에는 저장에 되는데 post 엔티티에는 저장이 안된다... 왜일까
+        System.out.println("[Info][productCreate SERVICE] post.getImages() : " + post.getImages()); // [null]
+
+        return post.getId();
     }
 
     /*게시물 전체조회*/
@@ -44,6 +89,7 @@ public class PostService {
     }
 
 //    /*찜한 목록 조회*/
+//    @Transactional
 //    public List<PostDto.PostDetailDto> getAllPostInfoLiked(Long memberId) {
 //        return postRepository.findAllListOfLiked(memberId).stream()
 //                .map(post -> new PostDto.PostDetailDto(post)).collect(Collectors.toList());
