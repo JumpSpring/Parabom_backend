6f400aeca5daf0981a310d6be91d38101ccb230c
sujin <osjkate@sookmyung.ac.kr>
2022-12-28 15:59:32 +0900

    거래 상태 변경, 게시물 수정, 삭제

--- src/main/java/taveSpring/parabom/Service/PostService.java
+++ src/main/java/taveSpring/parabom/Service/PostService.java
@@ -11,6 +11,9 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import static taveSpring.parabom.Controller.Dto.PostDto.*;
+
+
 
 @Service
 @RequiredArgsConstructor
@@ -49,4 +52,30 @@ public class PostService {
 //                .map(post -> new PostDto.PostDetailDto(post)).collect(Collectors.toList());
 //    }
 
+
+    /*구매 상태 변경*/
+    @Transactional
+    public void changeFinOrIng(Long id, ModifyFinOrIngRequest request) {
+        // 상태변경 기능
+        Post post = postRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("게시물 정보가 없습니다."));
+        post.updateFinOrIng(request.getFinOrIng());
+    }
+
+    /*게시물 삭제*/
+    @Transactional
+    public void postDelete(Long id) {
+        Post post = postRepository.findById(id)
+                        .orElseThrow(()-> new IllegalArgumentException("게시물 정보가 없습니다."));
+        postRepository.delete(post);
+    }
+
+    /*게시물 수정*/
+    @Transactional
+    public void postChange(Long id, ModifyRequest request) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("게시물 정보가 없습니다."));
+        post.update(request.getPrice(), request.getOpenOrNot(), request.getStatus(),
+                request.getDirectOrDel(), request.getCategory(), request.getHashtag());
+    }
 }
