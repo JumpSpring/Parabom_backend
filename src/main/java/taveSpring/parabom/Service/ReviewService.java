aa3a4c803057b14850f221a7cb8f2b0041d8f408
Soomin Lim <augustth511@gmail.com>
2022-12-29 15:36:11 +0900

    feat: 후기 수정 기능 구현

--- src/main/java/taveSpring/parabom/Service/ReviewService.java
+++ src/main/java/taveSpring/parabom/Service/ReviewService.java
@@ -3,11 +3,12 @@ package taveSpring.parabom.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import taveSpring.parabom.Controller.Dto.ReviewDto;
 import taveSpring.parabom.Domain.Member;
 import taveSpring.parabom.Domain.Review;
 import taveSpring.parabom.Repository.MemberRepository;
 import taveSpring.parabom.Repository.ReviewRepository;
+
+import static taveSpring.parabom.Controller.Dto.ReviewDto.*;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +17,9 @@ public class ReviewService {
     public final MemberRepository memberRepository;
     public final ReviewRepository reviewRepository;
 
-    /*리뷰 등록*/
+    /*후기 등록*/
     @Transactional
-    public ReviewDto.IdResponse saveReview(ReviewDto.ReviewCreateDto dto) {
-
+    public IdResponse saveReview(ReviewCreateDto dto) {
         Review review = dto.toEntity();
         review.setSenderType(dto.getSenderType());
 
@@ -30,9 +30,19 @@ public class ReviewService {
         review.setMembers(recipient, sender);
 
         reviewRepository.save(review);
-        return ReviewDto.IdResponse.of(review);
+        return IdResponse.of(review);
     }
 
+    /*후기 수정*/
+    @Transactional
+    public void updateReview(Long id, ReviewModifyDto dto) {
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("후기 정보가 없습니다."));
+
+        review.update(dto.getItemName(), dto.getText(), dto.getStarPoint());
+    }
+
+    /*후기 삭제*/
     @Transactional
     public void deleteReview(Long id) {
         Review review = reviewRepository.findById(id)
