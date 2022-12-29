10adf3ee04b6d4b5382c0a92aaf733b9f0cf66ac
Jeongyoon Song <sjyjudy710@naver.com>
2022-12-30 00:26:52 +0900

    fix: controller 충돌 해결

--- src/main/java/taveSpring/parabom/Controller/PostController.java
+++ src/main/java/taveSpring/parabom/Controller/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 import taveSpring.parabom.Controller.Dto.PostDto;
 import taveSpring.parabom.Controller.Response.BasicResponse;
 import taveSpring.parabom.Controller.Response.CommonResponse;
+import taveSpring.parabom.Service.PostLikesService;
 import taveSpring.parabom.Service.PostService;
 
 import java.security.Principal;
@@ -19,6 +20,7 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final PostLikesService postLikesService;
 
     /* 게시물 상세조회*/
     // TODO : Member 객체 넣기 완료
@@ -40,6 +42,36 @@ public class PostController {
             model.addAttribute("errorMessage : ", "게시물 등록 중 에러 발생!");
         }
         return ResponseEntity.ok().build();
+    }
+
+    /*게시글 찜하기 클릭*/
+    // TODO : 로그인한 사용자 정보 불러오기
+    @PostMapping("/addHeart")
+    public ResponseEntity<? extends BasicResponse> clickPostLike(
+            @RequestParam("memberId") Long memberId,
+            @RequestParam("postId") Long postId) throws Exception
+    {
+        postLikesService.clickPostLikes(memberId, postId);
+        return ResponseEntity.ok().build();
+    }
+
+    /*찜한 목록 조회*/
+    // TODO : 로그인한 사용자 정보 불러오기
+    @GetMapping("/likeList")
+    public ResponseEntity<? extends BasicResponse> allPostLiked(@RequestParam("memberId") Long memberId) throws Exception {
+        return ResponseEntity.ok().body(new CommonResponse<List>(postService.getAllPostInfoLiked(memberId)));
+    }
+
+    /*전체 조회*/
+    @GetMapping("/allList")
+    public ResponseEntity<? extends BasicResponse> allPosts() {
+        return ResponseEntity.ok().body(new CommonResponse<List>(postService.getAllPostInfo()));
+    }
+
+    /*카테고리 조회*/
+    @GetMapping("/category")
+    public ResponseEntity<? extends BasicResponse> findListByCategory(@RequestParam("categoryName") String categoryName) throws Exception {
+        return ResponseEntity.ok().body(new CommonResponse<List>(postService.getAllPostInfoByCategory(categoryName)));
     }
 
 
