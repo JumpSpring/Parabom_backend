1631e2998d7fd122b049d6d8b286757d332e2bf1
TaegeonKim <aksen5240@gmail.com>
2023-01-07 15:44:52 +0900

    testcode add

--- src/test/java/taveSpring/parabom/Service/ReviewServiceTest.java
+++ src/test/java/taveSpring/parabom/Service/ReviewServiceTest.java
@@ -15,6 +15,8 @@ import taveSpring.parabom.Domain.ReviewSenderType;
 import taveSpring.parabom.Repository.MemberRepository;
 import taveSpring.parabom.Repository.ReviewRepository;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -98,4 +100,52 @@ public class ReviewServiceTest {
                 .profile(profile)
                 .build();
     }
+
+    // 추가 코드 //
+
+    // 전체 후기 조회 테스트코드 작성, getReviewList
+    @Test
+    public void 후기_전체_조회() {
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        ReviewCreateDto dto1 = createDto(member1.getId(), "판매자", "iphone13", "감사합니다!", 5);
+        ReviewCreateDto dto2 = createDto(member1.getId(), "판매자", "iphone13", "감사합니다!", 3);
+
+        reviewService.saveReview(dto1, member2.getId());
+        reviewService.saveReview(dto2, member2.getId());
+
+        List<ReviewDto.ReviewListDto> reviewList = reviewService.getReviewList(member2.getId());
+
+        assertEquals(2, reviewList.size());
+        assertEquals("판매자", reviewList.get(0).getSenderType());
+        assertEquals("판매자", reviewList.get(1).getSenderType());
+        assertEquals("iphone13", reviewList.get(0).getItemName());
+        assertEquals("iphone13", reviewList.get(1).getItemName());
+        assertEquals("감사합니다!", reviewList.get(0).getText());
+        assertEquals("감사합니다!", reviewList.get(1).getText());
+        assertEquals(5, reviewList.get(0).getStarPoint());
+        assertEquals(3, reviewList.get(1).getStarPoint());
+
+    }
+
+    // 별점 조회
+    @Test
+    public void 후기_별점_조회() {
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        ReviewCreateDto dto1 = createDto(member1.getId(), "판매자", "iphone13", "감사합니다!", 5);
+        ReviewCreateDto dto2 = createDto(member1.getId(), "판매자", "iphone13", "감사합니다!", 3);
+
+        reviewService.saveReview(dto1, member1.getId());
+        reviewService.saveReview(dto2, member2.getId());
+
+
+        List<ReviewDto.ReviewListDto> reviewList = reviewService.getReviewList(member2.getId());
+
+        assertEquals(2, reviewList.size());
+        assertEquals(3, reviewService.getStarPoint(member2.getId()));
+
+    }
+
+    // 추가 코드 //
 }
